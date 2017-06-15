14f70a19d89c9b8b28eec5beb6eb6bfac8503991
Michael Pearson <mpearson658@googlemail.com>
2017-06-15 00:37:08 -0400

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'comments#index'
 
   # Example of regular route:
   get '/comments/:id' => 'comments#index'
